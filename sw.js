b94e5eb377ef999f5fea99b07ee833c8617785fc
Jared Piro <jpiro@v3.io>
2020-07-02 23:25:45 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1f4bb39c283d063b9e07.js"
+    "url": "webpack-runtime-a8104dd7e4741bfb5b5c.js"
   },
   {
     "url": "framework-381539f6286ce6c8ee65.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "24724b73e051747280a4d317d554eec7"
+    "revision": "bd65637104c517b2509f13deca746ed1"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e2936fc541441221b859.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "1495a00abb93de6a8a6161e21b988729"
+    "revision": "39c7cbd9cb2d267d4c6232c96e2bde70"
   },
   {
     "url": "manifest.webmanifest",
